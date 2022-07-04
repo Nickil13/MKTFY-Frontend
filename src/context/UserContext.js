4cb984d863130c766ea620521a44da371ef0886c
Nicki Lindstrom <lindstromn13@gmail.com>
2022-07-03 21:32:04 -0600

    Updates to context

--- src/context/UserContext.js
+++ src/context/UserContext.js
@@ -8,8 +8,12 @@ export const useUserContext = () => {
 };
 
 export const UserContextProvider = ({ children }) => {
-    const [token, setToken] = useState(sessionStorage.getItem("access_token"));
-    const [isAuthenticated, setIsAuthenticated] = useState(false);
+    // const [token, setToken] = useState(sessionStorage.getItem("access_token"));
+    const [user, setUser] = useState();
+    const [isAuthenticated, setIsAuthenticated] = useState(
+        sessionStorage.getItem("access_token")
+    );
+
     const [isLoading, setIsLoading] = useState(false);
     const [signupSuccess, setSignupSuccess] = useState(false);
     const [error, setError] = useState("");
@@ -18,46 +22,116 @@ export const UserContextProvider = ({ children }) => {
     const webAuth = new auth0js.WebAuth({
         domain: process.env.REACT_APP_DOMAIN,
         clientID: process.env.REACT_APP_CLIENT_ID,
-        //audience: `https://${process.env.REACT_APP_DOMAIN}/api/v2`,
-        scope: "read:current_user update:current_user_metadata",
+        // audience: `https://${process.env.REACT_APP_DOMAIN}/api/v2/`,
+        // scope: "read:current_user",
     });
 
-    /* Get token from session storage. If token, set authenticatd. */
+    /* Get token from session storage. If token, set authenticated. */
     React.useEffect(() => {
+        // webAuth.checkSession(
+        //     {
+        //         audience: `https://dev-9rx0zj33.us.auth0.com/api/v2/`,
+        //         responseType: "token id_token",
+        //         redirectUri: "http://localhost:3000",
+        //         scope: "read:current_user",
+        //     },
+        //     function (err, result) {
+        //         // use result.accessToken
+        //         var auth0Manage = new auth0js.Management({
+        //             domain: process.env.REACT_APP_DOMAIN,
+        //             token: result.accessToken,
+        //         });
+
+        //         console.log(auth0Manage);
+
+        //         return console.log(err, result);
+        //     }
+        // );
+
         let access_token = new URLSearchParams(
             document.location.hash.substring(1)
         ).get("access_token");
-        console.log("access token: ", access_token);
+
         if (access_token) {
-            sessionStorage.setItem("access_token", access_token);
-            setIsAuthenticated(true);
+            webAuth.parseHash(
+                { hash: window.location.hash },
+                function (err, authResult) {
+                    if (err) {
+                        return console.log(err);
+                    }
+                    console.log(authResult);
+
+                    var auth0Manage = new auth0js.Management({
+                        domain: process.env.REACT_APP_DOMAIN,
+                        token: authResult.accessToken,
+                    });
+                    webAuth.client.userInfo(
+                        authResult.accessToken,
+                        function (err, user) {
+                            if (err) return console.log(err);
+
+                            auth0Manage.getUser(
+                                user.sub,
+                                function (err, result) {
+                                    if (err) return console.log(err);
+                                    return console.log(result);
+                                }
+                            );
+                            return console.log(user);
+                        }
+                    );
+                    // webAuth.client.userInfo(
+                    //     authResult.accessToken,
+                    //     function (err, user) {
+                    //         // Now you have the user's information
+                    //         if (err) {
+                    //             return console.log(err);
+                    //         }
+                    //         // console.log(user.sub.split("|"[1]))
+                    //         // auth0Manage.getUser(
+                    //         //     "auth0|62b8182548b94e0a9efbf9fc",
+                    //         //     (res) => {
+                    //         //         console.log(res);
+                    //         //     }
+                    //         // );
+                    //         // console.log(user1);
+                    //         return console.log(user);
+                    //     }
+                    // );
+                }
+            );
         }
+
+        // let access_token = new URLSearchParams(
+        //     document.location.hash.substring(1)
+        // ).get("access_token");
+
+        // if (access_token) {
+        //     sessionStorage.setItem("access_token", access_token);
+        //     setIsAuthenticated(true);
+        //     console.log("access token saved");
+        // }
     }, []);
 
-    React.useEffect(() => {
-        if (!isAuthenticated && token) {
-            setIsAuthenticated(true);
-        }
-    }, [token]);
-
     const login = (email, password) => {
-        setIsLoading(true);
         webAuth.login(
             {
                 responseType: "token",
                 realm: process.env.REACT_APP_REALM,
                 email: "nickitest@gmail.com",
                 password: "@Testing1",
+                // audience: `https://${process.env.REACT_APP_DOMAIN}/api/v2/`,
                 redirectUri: "http://localhost:3000/",
+                // scope: "read:current_user",
                 onRedirecting: function (done) {
-                    setIsLoading(false);
+                    // setIsLoading(false);
                     done();
                 },
             },
             (error) => {
                 console.log(error);
                 setError(error.description);
-                setIsLoading(false);
+                // setIsLoading(false);
             }
         );
     };
@@ -71,22 +145,24 @@ export const UserContextProvider = ({ children }) => {
     //firstname,lastname,email,phoneNumber,address,city,password
     const signup = (userInfo) => {
         const {
-            firstname,
-            lastname,
+            firstName,
+            lastName,
             email,
             phoneNumber,
             address,
             city,
             password,
         } = userInfo;
+        console.log(userInfo);
         webAuth.signup(
             {
                 connection: process.env.REACT_APP_REALM,
                 email,
                 password,
+
                 user_metadata: {
-                    firstname,
-                    lastname,
+                    firstName,
+                    lastName,
                     phoneNumber,
                     address,
                     city,
--- src/index.js
+++ src/index.js
@@ -4,7 +4,6 @@ import "./styles/index.css";
 import App from "./App";
 import { ModalContextProvider } from "./context/ModalContext";
 import { UserContextProvider } from "./context/UserContext";
-
 const root = ReactDOM.createRoot(document.getElementById("root"));
 
 root.render(
