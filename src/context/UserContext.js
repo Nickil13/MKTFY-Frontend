b20ca0986f9fa160fec4573a505dbebe1d0108b4
Nicki Lindstrom <lindstromn13@gmail.com>
2022-07-06 22:13:27 -0600

    A few updates to UserContext to get auth

--- src/context/UserContext.js
+++ src/context/UserContext.js
@@ -1,5 +1,6 @@
 import React, { useState, useContext } from "react";
 import auth0js from "auth0-js";
+import axios from "axios";
 
 const UserContext = React.createContext();
 
@@ -20,8 +21,10 @@ export const UserContextProvider = ({ children }) => {
     const webAuth = new auth0js.WebAuth({
         domain: process.env.REACT_APP_DOMAIN,
         clientID: process.env.REACT_APP_CLIENT_ID,
-        //audience: `https://${process.env.REACT_APP_DOMAIN}/api/v2`,
-        scope: "read:current_user update:current_user_metadata",
+        // audience: `https://${process.env.REACT_APP_DOMAIN}/api/v2/`,
+        audience: "http://marketforyou.com",
+
+        // scope: "read:current_user update:current_user_metadata",
     });
 
     /* Get token from session storage. If token, set authenticatd. */
@@ -29,12 +32,48 @@ export const UserContextProvider = ({ children }) => {
         let access_token = new URLSearchParams(
             document.location.hash.substring(1)
         ).get("access_token");
+
         console.log("access token: ", access_token);
+        // checkApi(access_token);
+
         if (access_token) {
             sessionStorage.setItem("access_token", access_token);
             setIsAuthenticated(true);
         }
     }, []);
+
+    // function checkApi(access_token) {
+    async function checkApi(access_token) {
+        console.log("access token: ", access_token);
+
+        // try {
+        //     const res = await axios.get(
+        //         `http://marketforyouyh-env.eba-fqgiudi2.ca-central-1.elasticbeanstalk.com/api/Listing/all`,
+        //         {
+        //             headers: {
+        //                 Authorization: `Bearer ${access_token}`,
+        //             },
+        //         }
+        //     );
+        //     console.log(res);
+        // } catch (error) {
+        //     console.log(error);
+        // }
+        let header = new Headers({
+            Authorization: `Bearer ${access_token}`,
+            "Content-Type": "application/json",
+        });
+        fetch(
+            `http://marketforyouyh-env.eba-fqgiudi2.ca-central-1.elasticbeanstalk.com/api/Listing/all`,
+            {
+                headers: header,
+                mode: "no-cors",
+            }
+        )
+            .then((res) => res.json())
+            .then((data) => console.log(data))
+            .catch((error) => console.log(error));
+    }
 
     // React.useEffect(() => {
     //     if (!isAuthenticated && token) {
