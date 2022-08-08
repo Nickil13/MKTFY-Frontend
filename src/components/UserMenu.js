92646de43dcc64ed9536336e0f8eb15d9015a03a
Nicki Lindstrom <lindstromn13@gmail.com>
2022-08-08 17:53:20 -0600

    Updated user menu for scrollability at smaller window sizes.

--- src/components/UserMenu.js
+++ src/components/UserMenu.js
@@ -35,46 +35,49 @@ export default function UserMenu() {
                         </div>
                         <h2 className="font-bold">{`${user?.firstName} ${user?.lastName}`}</h2>
                     </div>
-
-                    <div className="mb-24">
-                        <h3 className="text-xs font-bold pb-3.5 px-3.5">
-                            Settings
-                        </h3>
-                        <ul>
-                            {USER_MENU_LINKS.map(({ name, path }, index) => {
-                                return (
-                                    <li
-                                        className="p-3.5 hover:bg-beige-200"
-                                        key={index}
-                                    >
-                                        <Link to={path}>{name}</Link>
-                                        {name === "My Listings" && (
-                                            <span className="bg-purple-500 text-white px-4 py-2.5 rounded-lg ml-4">
-                                                2
-                                            </span>
-                                        )}
-                                    </li>
-                                );
-                            })}
-                        </ul>
+                    <div className="overflow-y-auto max-h-menu hide-scrollbar overscroll-y-contain">
+                        <div className="mb-24">
+                            <h3 className="text-xs font-bold pb-3.5 px-3.5">
+                                Settings
+                            </h3>
+                            <ul>
+                                {USER_MENU_LINKS.map(
+                                    ({ name, path }, index) => {
+                                        return (
+                                            <li
+                                                className="p-3.5 hover:bg-beige-200"
+                                                key={index}
+                                            >
+                                                <Link to={path}>{name}</Link>
+                                                {name === "My Listings" && (
+                                                    <span className="bg-purple-500 text-white px-4 py-2.5 rounded-lg ml-4">
+                                                        2
+                                                    </span>
+                                                )}
+                                            </li>
+                                        );
+                                    }
+                                )}
+                            </ul>
+                        </div>
+                        <div className="mb-12">
+                            <h3 className="text-xs font-bold p-3.5">Help</h3>
+                            <ul>
+                                <li className="p-3.5 hover:bg-beige-200 cursor-pointer">
+                                    <Link to="FAQ">FAQ</Link>
+                                </li>
+                                <li className="p-3.5 hover:bg-beige-200 cursor-pointer">
+                                    Contact Us
+                                </li>
+                            </ul>
+                        </div>
+                        <button
+                            className="flex justify-between items-center p-3.5 text-red w-full"
+                            onClick={handleSignout}
+                        >
+                            Sign Out <SignoutIcon />
+                        </button>
                     </div>
-                    <div className="mb-12">
-                        <h3 className="text-xs font-bold p-3.5">Help</h3>
-                        <ul>
-                            <li className="p-3.5 hover:bg-beige-200 cursor-pointer">
-                                <Link to="FAQ">FAQ</Link>
-                            </li>
-                            <li className="p-3.5 hover:bg-beige-200 cursor-pointer">
-                                Contact Us
-                            </li>
-                        </ul>
-                    </div>
-                    <button
-                        className="flex justify-between items-center p-3.5 text-red w-full"
-                        onClick={handleSignout}
-                    >
-                        Sign Out <SignoutIcon />
-                    </button>
                 </div>
             </Dropdown>
             <button className="mr-9" onClick={() => setUserMenuShowing(true)}>
